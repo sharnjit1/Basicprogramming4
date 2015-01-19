380e48a0e321a98bb35564b3bba387255583033f
sharnjit1 <montystar0@gmail.com>
2015-01-19 12:43:55 +0530

    Basicprogramming4
    Java

---
+++ src/basicprogramming4/Basicprogramming4.java
@@ -0,0 +1,25 @@
+
+package basicprogramming4;
+
+import java.util.Random;
+
+
+public class Basicprogramming4 {
+
+    
+    public static void main(String[] args) {
+      
+        
+             String something = "HAH im having a great day";
+           
+            Random ok = new Random();
+            
+            System.out.println(ok.nextInt()+ " "+ ok.nextInt());
+            
+      
+      
+    }
+}
+
+
+
